bc4a396967d9f5597eb34c1e6aa7737e6bdf919c
LazyEngineerToBe <alexandre.francoeur.3@etsmtl.net>
2020-02-11 22:27:07 -0500

    WIP, compiles and runs. Error on param init.

--- jog_controller/src/twist_to_jog_frame.cpp
+++ jog_controller/src/twist_to_jog_frame.cpp
@@ -4,7 +4,7 @@
 
 #include <std_msgs/Bool.h>
 #include <std_msgs/String.h>
-#include <jog_controller/twist_to_jog_frame.h>
+#include "twist_to_jog_frame.h"
 #include "jog_msgs/JogFrame.h"
 
 namespace jog_controller
@@ -14,41 +14,27 @@ TwistToJogFrame::TwistToJogFrame()
 {
   ros::NodeHandle nh;
   jog_frame_pub_ = nh.advertise<jog_msgs::JogFrame>("jog_frame", 1);
-  set_target_frame_srv_ = nh.advertiseService("set_target_frame", setTargetFrame);
-  set_target_link_srv_ = nh.advertiseService("set_target_link", setTargetLink);
-  group_name_ = nh.getParam("~group_name", group_name_);
-  link_name_ = nh.getParam("~link_name", link_name_);
-  frame_id_ = nh.getParam("~frame_id", frame_id_);
-  rotate_axes_ = nh.getParam("~rotate_axes", rotate_axes_);
-  rotation_matrix_ = arma::mat33(nh.getParam("~rotation_matrix", rotation_matrix_));
-  dominant_axis_mode_ = nh.getParam("~dominant_axis_mode", dominant_axis_mode_);
-  scale_linear_ = nh.getParam("~scale/linear", scale_linear_);
-  scale_angular_ = nh.getParam("~scale/angular", scale_angular_);
-  sub_topic_ = nh.getParam("~sub_topic", sub_topic_);
-  controller_enabled_ = nh.getParam("~controller_enabled", controller_enabled_);
+  // set_target_frame_srv_ = nh.advertiseService("set_target_frame", setTargetFrame);
+  // set_target_link_srv_ = nh.advertiseService("set_target_link", setTargetLink);
+  nh.getParam("/jog_frame_node/group_names", group_names_);
+  nh.getParam("/jog_frame_node/link_names", link_names_);
+  nh.getParam("group_name", group_name_);
+  nh.getParam("link_name", link_name_);
+  nh.getParam("frame_id", frame_id_);
+  nh.getParam("rotate_axes", rotate_axes_);
+  std::string str_rotation_matrix_;
+  nh.getParam("rotation_matrix", str_rotation_matrix_); 
+  rotation_matrix_ = arma::mat(str_rotation_matrix_);
+  nh.getParam("dominant_axis_mode", dominant_axis_mode_);
+  nh.getParam("scale_linear", scale_linear_);
+  nh.getParam("scale_angular", scale_angular_);
+  nh.getParam("sub_topic", sub_topic_);
+  nh.getParam("controller_enabled", controller_enabled_);
+  twist_sub_ = nh.subscribe(sub_topic_, 10, &TwistToJogFrame::twist_cb, this);
+  ros::topic::waitForMessage<geometry_msgs::Twist>(sub_topic_);
 }
 
-// void JogFramePanel::updateFrame()
-// {
-//   typedef std::vector<std::string> V_string;
-//   V_string frames;
-//   vis_manager_->getTFClient()->getFrameStrings( frames );
-//   std::sort(frames.begin(), frames.end());
-//   frame_cbox_->clear();
-//   for (V_string::iterator it = frames.begin(); it != frames.end(); ++it )
-//   {
-//     const std::string& frame = *it;
-//     if (frame.empty())
-//     {
-//       continue;
-//     }
-//     frame_cbox_->addItem(it->c_str());
-//   }
-//   frame_cbox_->setCurrentIndex(0);
-//   frame_id_ = frame_cbox_->currentText().toStdString();
-// }
-
-void TwistToJogFrame::keepOnlyDominantAxis(const arma::vec6 &twist_p)
+void TwistToJogFrame::keepOnlyDominantAxis(arma::vec6 &twist_p)
 {
   double min = twist_p.min();
   double max = twist_p.max();
@@ -56,7 +42,7 @@ void TwistToJogFrame::keepOnlyDominantAxis(const arma::vec6 &twist_p)
   if(min_max > 0){
     arma::uword i = twist_p.index_max();
     twist_p.zeros();
-    twist_p->at(i) = max;
+    twist_p.at(i) = max;
   } else {
     arma::uword i = twist_p.index_min();
     twist_p.zeros();
@@ -64,7 +50,7 @@ void TwistToJogFrame::keepOnlyDominantAxis(const arma::vec6 &twist_p)
   }
 }
 
-void TwistToJogFrame::rotateAxes(const arma::vec6 &twist_p)
+void TwistToJogFrame::rotateAxes(arma::vec6 &twist_p)
 {
   arma::vec linear = arma::vec3({twist_p.at(0), twist_p.at(1), twist_p.at(2)});
   arma::vec angular = arma::vec3({twist_p.at(3), twist_p.at(4), twist_p.at(5)});
@@ -79,13 +65,13 @@ void TwistToJogFrame::rotateAxes(const arma::vec6 &twist_p)
   }
 }
 
-bool TwistToJogFrame::hasDuplicate(const arma::vec6 const &twist_p)
+bool TwistToJogFrame::hasDuplicate(const arma::vec6 &twist_p)
 {
   arma::vec tmp = arma::unique(twist_p);
   return size(tmp) != size(twist_p);
 }
 
-void TwistToJogFrame::scaleCommand(const arma::vec6 &twist_p)
+void TwistToJogFrame::scaleCommand(arma::vec6 &twist_p)
 {
   for(arma::uword i = 0; i < 3; i++)
   {
@@ -97,7 +83,7 @@ void TwistToJogFrame::scaleCommand(const arma::vec6 &twist_p)
   }
 }
 
-void TwistToJogFrame::publish(const geometry_msgs::Twist::ConstPtr &twist)
+void TwistToJogFrame::twist_cb(const geometry_msgs::TwistConstPtr &twist)
 {
   boost::mutex::scoped_lock lock(mutex_);
 
@@ -108,7 +94,7 @@ void TwistToJogFrame::publish(const geometry_msgs::Twist::ConstPtr &twist)
   msg.group_name = group_name_;
   msg.link_name = target_link_id_;
 
-  arma::vec v_twist = arma::vec6({twist->linear.x, twist->linear.y, twist->linear.z, twist->angular.x, twist->angular.y, twist->angular.z});
+  arma::vec6 v_twist = arma::vec6({twist->linear.x, twist->linear.y, twist->linear.z, twist->angular.x, twist->angular.y, twist->angular.z});
 
   if(dominant_axis_mode_)
   {
@@ -138,25 +124,24 @@ void TwistToJogFrame::publish(const geometry_msgs::Twist::ConstPtr &twist)
       jog_frame_pub_.publish(msg);
     }
   }  
-  }
-}  
+  }  
 
 //callback for controller_enable service
 bool TwistToJogFrame::setControllerStatus(std_msgs::Bool &status)
 {
-  this.controller_enabled_ = status.data;
+  controller_enabled_ = status.data;
   return true;
 }
 
 bool TwistToJogFrame::setTargetFrame(std_msgs::String &frame)
 {
-  this.frame_id_ = frame.data;
+  frame_id_ = frame.data;
   return true;
 }
 
 bool TwistToJogFrame::setTargetLink(std_msgs::String &link)
 {
-  this.target_link_id_ = link.data;
+  target_link_id_ = link.data;
   return true;
 }
 
@@ -168,7 +153,7 @@ bool TwistToJogFrame::setTargetLink(std_msgs::String &link)
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "twist_to_jog_frame");
-  TwistToJogFrame::TwistToJogFrame();
+  jog_controller::TwistToJogFrame node;
 
   ros::Rate loop_rate(10);
   while ( ros::ok() )
--- jog_controller/src/twist_to_jog_frame.h
+++ jog_controller/src/twist_to_jog_frame.h
@@ -5,6 +5,10 @@
 #include <boost/thread.hpp>
 #include <armadillo>
 #include <geometry_msgs/Twist.h>
+#include <std_msgs/Bool.h>
+#include <std_msgs/String.h>
+#include <string>
+#include "jog_msgs/JogFrame.h"
 
 namespace jog_controller
 {
@@ -12,33 +16,36 @@ class TwistToJogFrame
   {
   public:
     TwistToJogFrame();
-    void publish(const geometry_msgs::Twist::ConstPtr &twist);
+    void twist_cb(const geometry_msgs::TwistConstPtr &twist);
     
   protected:
-    std::vector<std::string> group_name_;
-    std::vector<std::string> link_name_;
+    std::vector<std::string> group_names_;
+    std::vector<std::string> link_names_;
+    std::string group_name_;
+    std::string link_name_;
     std::string frame_id_;
     std::string target_link_id_;
     std::string sub_topic_;
-    std::string scale_linear_;
-    std::string scale_angular_;
+    double scale_linear_;
+    double scale_angular_;
     boost::mutex mutex_;
     bool rotate_axes_;
     bool dominant_axis_mode_;
     bool controller_enabled_;
     ros::Publisher jog_frame_pub_;
-    arma::mat33 rotation_matrix_;
+    arma::mat rotation_matrix_;
     ros::ServiceServer set_target_frame_srv_;
     ros::ServiceServer set_target_link_srv_;
 
-    void keepOnlyDominantAxis(const arma::vec6 &twist_p);
-    void rotateAxes(const arma::vec6 &twist_p);
-    bool hasDuplicate(const arma::vec6 const &twist_p);
-    void scaleCommand(const arma::vec6 &twist_p);
+    void keepOnlyDominantAxis(arma::vec6 &twist_p);
+    void rotateAxes(arma::vec6 &twist_p);
+    bool hasDuplicate(const arma::vec6 &twist_p);
+    void scaleCommand(arma::vec6 &twist_p);
 
     bool setControllerStatus(std_msgs::Bool &status);
     bool setTargetFrame(std_msgs::String &frame);
     bool setTargetLink(std_msgs::String &link);
+    ros::Subscriber twist_sub_;
   };
 
 }  // namespace jog_controller
